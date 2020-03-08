2b628ae8db63d440666ce3530e07286e8c0ed409
triton <triton.68@mail.ru>
2020-03-08 21:13:50 +0300

    feat: valiadtion expression

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.split(' ');
+  if (isDevideByZero(expr)) throw "TypeError: Division by zero.";
+  if (!isBracketsPaired(expr)) throw "ExpressionError: Brackets must be paired";
 }
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
+}
+
+
+
+function isDevideByZero(arr) {
+  answer = false;
+  arr.forEach((elem, ind) => {
+    if (elem == '/' && arr[ind + 1] == '0') answer = true;
+  });
+  return answer;
+}
+
+function isBracketsPaired(arr) {
+  let outArr = arr.join('').split('');
+  let left = 0;
+  let right = 0;
+  outArr.forEach(elem => {
+    if (elem === '(') left++;
+    if (elem === ')') right++;
+  });
+  if (left === right) {
+    return true
+  } else {
+    return false
+  }
 }
